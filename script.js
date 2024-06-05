b256ea5def0a750e105bec1f6fb2c4ae2154902a
useronlineid <dmcode1010@gmail.com>
2024-06-05 11:47:00 +0700

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.getElementById('saveImage').addEventListener('click', function() {
-    const referenceNumber = document.getElementById('referenceNumber').value || "AMPim191";
-    const recipientName = document.getElementById('recipientName').value || "สนชมา กสนกา";
-    const accountNumber = document.getElementById('accountNumber').value || "5502675802";
-    const amount = document.getElementById('amount').value || "408,309.20 บาท";
-    const transactionDate = document.getElementById('transactionDate').value || "30/05/2567 17:47:05";
-    const notes = document.getElementById('notes').value;
-
+    const referenceNumber = document.getElementById('referenceNumber').value || "-";
+    const recipientName = document.getElementById('recipientName').value || "-";
+    const accountNumber = document.getElementById('accountNumber').value || "-";
+    const amount = document.getElementById('amount').value || "-";
+    const transactionDate = document.getElementById('transactionDate').value || "-";
+    const notes = document.getElementById('notes').value || "บัญชีของสมาชิกไม่ตรงกับข้อมูลในระบบ ธนาคารทำการโอนซ้ำหลายรอบ ไม่สามารถโอนเงินเข้าได้ ตามกฎระเบียบของธนาคาร กฎหมายความมั่นคงของกองทุนสมาชิก ถูกอายัดชั่วคราว";
+ 
     // สร้างหน้าใหม่และเปิด
     const outputWindow = window.open('', '', 'width=1500,height=721');
     outputWindow.document.write(`
@@ -19,7 +19,7 @@ document.getElementById('saveImage').addEventListener('click', function() {
                 <p style="position: absolute; top: 167px; left: 700px; color:#656565; font-size: 28px;">${referenceNumber}</p>
                 <p style="position: absolute; top: 222px; left: 700px; color:#656565; font-size: 28px;">${recipientName}</p>
                 <p style="position: absolute; top: 278px; left: 700px; color:#656565; font-size: 28px;">${accountNumber}</p>
-                <p style="position: absolute; top: 334px; left: 700px; color:#656565; font-size: 28px;">${amount}</p>
+                <p style="position: absolute; top: 334px; left: 700px; color:#656565; font-size: 28px;">${amount} บาท</p>
                 <p style="position: absolute; top: 387px; left: 700px; color:#656565; font-size: 28px;">${transactionDate}</p>
                 <p style="position: absolute; top: 449px; left: 528px; color:red; white-space: pre-line; font-size: 26px;width: 42%;">${notes}</p>
             </div>
