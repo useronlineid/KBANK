103e3567d2f5bed0313c6f9c764c34ca5f718d78
useronlineid <dmcode1010@gmail.com>
2024-07-02 17:29:39 +0700

    script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.getElementById('saveImage').addEventListener('click', function() {
     const accountNumber = document.getElementById('accountNumber').value || "-";
     const Payeeaccount = document.getElementById('Payeeaccount').value || "-";
     const savings = document.getElementById('savings').value || "-";
-    const time = document.getElementById('time').value || "-";
+    const Dateandtime = document.getElementById('Dateandtime').value || "-";
     const notes = document.getElementById('notes').value || "บัญชีของสมาชิกไม่ตรงกับข้อมูลในระบบ ธนาคารทำการโอนซ้ำหลายรอบ ไม่สามารถโอนเงินเข้าได้ ตามกฎระเบียบของธนาคาร กฎหมายความมั่นคงของกองทุนสมาชิก ถูกอายัดชั่วคราว";
  
     // สร้างหน้าใหม่และเปิด
@@ -20,7 +20,7 @@ document.getElementById('saveImage').addEventListener('click', function() {
                 <p style="position: absolute; top: 222px; left: 700px; color:#656565; font-size: 28px;">${accountNumber}</p>
                 <p style="position: absolute; top: 278px; left: 700px; color:#656565; font-size: 28px;">${Payeeaccount}</p>
                 <p style="position: absolute; top: 334px; left: 700px; color:#656565; font-size: 28px;">${savings} บาท</p>
-                <p style="position: absolute; top: 387px; left: 700px; color:#656565; font-size: 28px;">${time}</p>
+                <p style="position: absolute; top: 387px; left: 700px; color:#656565; font-size: 28px;">${Dateandtime}</p>
                 <p style="position: absolute; top: 451.4px; left: 528px; color:red; white-space: pre-line; font-size: 26px;width: 42%;">${notes}</p>
             </div>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"></script>
