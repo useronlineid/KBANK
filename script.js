9670f6db47a468335efae5a06640a614e0240cd4
useronlineid <dmcode1010@gmail.com>
2024-07-02 17:03:16 +0700

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.getElementById('saveImage').addEventListener('click', function() {
-    const referenceNumber = document.getElementById('referenceNumber').value || "-";
-    const recipientName = document.getElementById('recipientName').value || "-";
+    const User = document.getElementById('User').value || "-";
     const accountNumber = document.getElementById('accountNumber').value || "-";
-    const amount = document.getElementById('amount').value || "-";
-    const transactionDate = document.getElementById('transactionDate').value || "-";
+    const Payeeaccount = document.getElementById('Payeeaccount').value || "-";
+    const savings = document.getElementById('savings').value || "-";
+    const time = document.getElementById('time').value || "-";
     const notes = document.getElementById('notes').value || "บัญชีของสมาชิกไม่ตรงกับข้อมูลในระบบ ธนาคารทำการโอนซ้ำหลายรอบ ไม่สามารถโอนเงินเข้าได้ ตามกฎระเบียบของธนาคาร กฎหมายความมั่นคงของกองทุนสมาชิก ถูกอายัดชั่วคราว";
  
     // สร้างหน้าใหม่และเปิด
@@ -16,11 +16,11 @@ document.getElementById('saveImage').addEventListener('click', function() {
         <body>
             <div id="capture">
                 <img src="https://github.com/useronlineid/KBANK/blob/main/3.jpg?raw=true" alt="Image">
-                <p style="position: absolute; top: 167px; left: 700px; color:#656565; font-size: 28px;">${referenceNumber}</p>
-                <p style="position: absolute; top: 222px; left: 700px; color:#656565; font-size: 28px;">${recipientName}</p>
-                <p style="position: absolute; top: 278px; left: 700px; color:#656565; font-size: 28px;">${accountNumber}</p>
-                <p style="position: absolute; top: 334px; left: 700px; color:#656565; font-size: 28px;">${amount} บาท</p>
-                <p style="position: absolute; top: 387px; left: 700px; color:#656565; font-size: 28px;">${transactionDate}</p>
+                <p style="position: absolute; top: 167px; left: 700px; color:#656565; font-size: 28px;">${User}</p>
+                <p style="position: absolute; top: 222px; left: 700px; color:#656565; font-size: 28px;">${accountNumber}</p>
+                <p style="position: absolute; top: 278px; left: 700px; color:#656565; font-size: 28px;">${Payeeaccount}</p>
+                <p style="position: absolute; top: 334px; left: 700px; color:#656565; font-size: 28px;">${savings} บาท</p>
+                <p style="position: absolute; top: 387px; left: 700px; color:#656565; font-size: 28px;">${time}</p>
                 <p style="position: absolute; top: 451.4px; left: 528px; color:red; white-space: pre-line; font-size: 26px;width: 42%;">${notes}</p>
             </div>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"></script>
